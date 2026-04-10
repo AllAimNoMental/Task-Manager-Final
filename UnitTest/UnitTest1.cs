78e23ed1040171ee9ae19b3f619ad4cd5ec1107c
AllAimNoMental <luis.colmenares@students.snow.edu>
2026-04-10 16:08:05 -0600

    help with json

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -13,12 +13,18 @@ namespace UnitTest
         {
             TaskManager manager = new TaskManager();
 
-            WorkTask task = new WorkTask("math", Priority.High, new DateTime(2026 , 06 , 20));
+            WorkTask task = new WorkTask("math", Priority.High, new DateTime(2026, 06, 20));
 
-
+            manager.AddTask(task);
             Assert.Single(manager.Tasks);
             Assert.Equal(task, manager.Tasks[0]);
         }
+//Single → cantidad exacta
+//Equal → comparar valores
+//Contains → está en lista
+//DoesNotContain → no está
+//True / False → condiciones
+//NotNull → existe objeto
 
     }
 }
