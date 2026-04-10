4cf3351043455a6b71dee89b9ccfafc70eb507ee
AllAimNoMental <luis.colmenares@students.snow.edu>
2026-04-09 19:41:49 -0600

    unit test  i need to do and fis json

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using TaskManager1;
+using TaskBase1;
+using PersonalTask1;
+using WorkTask1;
+
+
+namespace UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void AddingTask()
+        {
+            TaskManager manager = new TaskManager();
+
+            WorkTask task = new WorkTask("math", Priority.High, new DateTime(2026 , 06 , 20));
+
+
+            Assert.Single(manager.Tasks);
+            Assert.Equal(task, manager.Tasks[0]);
+        }
+
+    }
+}
